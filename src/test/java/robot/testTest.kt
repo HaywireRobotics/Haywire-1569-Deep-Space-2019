e87f428038a3ffa568a349496dc092d078753266
MetaHerobrine1 <brenekharrison@gmail.com>
2019-01-30 14:05:04 -0700

    Updated testTest.kt

--- src/test/java/robot/testTest.kt
+++ src/test/java/robot/testTest.kt
@@ -24,15 +24,16 @@ class MyClass {
 class TestTest {
     val myClass = MyClass()
     var mockedMyClass: MyClass = mock()
-    // @Before
-    // fun setUp() {
-    //     mockedMyClass = mock()
-    // }
 
     init {
         myClass.doAThing()
         myClass.doADifferentThing()
     }
+    
+    // @Before
+    // fun setUp() {
+    //     mockedMyClass = mock()
+    // }
 
     @Test
     fun test_numero_uno() {
