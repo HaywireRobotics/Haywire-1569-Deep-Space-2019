82e728ca6f3df73894b04b813422a4e77a1a0e85
Cody Higgins <thecweeper@gmail.com>
2019-03-20 14:05:16 -0600

    Working on Targetting an Angle

--- src/main/java/frc/robot/commands/CargoArmToAngle.kt
+++ src/main/java/frc/robot/commands/CargoArmToAngle.kt
@@ -9,6 +9,7 @@ package frc.robot.commands
 
 import kotlin.comparisons.minOf
 import kotlin.math.abs
+import kotlin.math.round
 
 import edu.wpi.first.wpilibj.command.Command
 import frc.robot.Robot
@@ -22,23 +23,30 @@ class CargoArmToAngle(val targetAngle: Int): Command() {
     requires(Robot.m_intakeSubsystem)
     requires(Robot.m_sensorSubsystem)
   }
-  val maxArmPower: Double = 0.4
+  val maxArmPower: Double = 0.5
+  val iterationMultiplier: Double = 0.5
   var robotPower: Double = 0.0
+  var iterations: Double = 0.0
 
   // Called just before this Command runs the first time
-  override fun initialize () {}
+  override fun initialize () {
+    iterations = 0.0
+  }
 
   // Called repeatedly when this Command is scheduled to run
   override fun execute () {
     // println(Robot.m_sensorSubsystem.cargoNavX.pitch)
+    iterations += 1
+    println(round(iterations * iterationMultiplier).toString())
     robotPower = 0.0
     if (Robot.m_sensorSubsystem.cargoNavX.pitch < targetAngle) {
-      robotPower = -minOf(maxArmPower, (abs(Robot.m_sensorSubsystem.cargoNavX.pitch - targetAngle)/10).toDouble())
+      // robotPower = -minOf(maxArmPower, (abs(Robot.m_sensorSubsystem.cargoNavX.pitch - targetAngle)/10).toDouble())
       // Robot.m_intakeSubsystem.IntakeHinge.set(-minOf(maxArmPower, (abs(Robot.m_sensorSubsystem.cargoNavX.pitch - targetAngle)/10).toDouble()))
-    } else if (Robot.m_sensorSubsystem.cargoNavX.pitch > targetAngle) {
-      // robotPower = minOf(maxArmPower, (abs(Robot.m_sensorSubsystem.cargoNavX.pitch - targetAngle)/10).toDouble())
-      // Robot.m_intakeSubsystem.IntakeHinge.set(minOf(maxArmPower, (abs(Robot.m_sensorSubsystem.cargoNavX.pitch - targetAngle)/10).toDouble()))
       robotPower = 0.0
+    } else if (Robot.m_sensorSubsystem.cargoNavX.pitch > targetAngle) {
+      robotPower = minOf(maxArmPower, (abs(Robot.m_sensorSubsystem.cargoNavX.pitch - targetAngle)/(20 + (iterations * iterationMultiplier))).toDouble())
+      // Robot.m_intakeSubsystem.IntakeHinge.set(minOf(maxArmPower, (abs(Robot.m_sensorSubsystem.cargoNavX.pitch - targetAngle)/10).toDouble()))
+      // robotPower = 0.0
     } else {
       // Robot.m_intakeSubsystem.IntakeHinge.set(0.0)
       robotPower = 0.0
